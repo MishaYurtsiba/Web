74077ca4de36af2101b5eb03d8377875b53678b5
MishaYurtsiba <73959369+MishaYurtsiba@users.noreply.github.com>
2021-05-15 12:04:52 +0300

    Add files via upload

---
+++ JS_CLIK/app.js
@@ -0,0 +1,16 @@
+let div=document.getElementById("d2");
+let div2=document.getElementById("d4");
+
+
+function s1(){
+    div.style.display = "block";  
+}
+function s2(){
+    div.style.display = "none";
+}
+function s3(){
+    div2.style.display = "block";  
+}
+function s4(){
+    div2.style.display = "none";
+}
